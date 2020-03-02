ac464426923380a484728b743a3c2d7b6858e3eb
Fnowa <f.nowak@microsoft.wsei.edu.pl>
2020-03-02 18:32:04 +0100

    Początek

---
+++ Figury/FiguryLibrary/Trojkat.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace FiguryLibrary
+{
+    public class Trojkat
+    {
+        // dane obiektu - pola obiektu (fields)
+        double a, b, c; // this.a, this.b, this.c
+
+        // budow obiektu - konstruktor
+        public Trojkat() // Trojkat() - konstruktor domyślny
+        {
+            a = b = c = 1.0;
+        }
+        public Trojkat(double a, double b, double c)
+        {
+            this.a = a;
+            this.b = b;
+            this.c = c;
+        }
+    }
+}
